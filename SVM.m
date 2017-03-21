ddff8a4c9857c33790e0421f16a8dc9ce39d5e4f
ZJUZT <3120101111@zju.edu.cn>
2017-03-21 12:11:57 +0800

    linear SVM done

---
+++ SVM.m
@@ -0,0 +1,108 @@
+% reset rand seed
+rng('default');
+y = rand(1,5);
+
+% load training data
+[num_sample, p] = size(train_X);
+
+% parameters
+iter_num = 1;
+epoch = 1;
+learning_rate = 1e-3;
+
+loss_svm_test = zeros(iter_num, epoch);
+loss_svm_train = zeros(iter_num, epoch);
+accuracy_svm = zeros(iter_num, epoch);
+
+for i=1:iter_num
+    b = 0;
+    W = zeros(1,p);
+    loss_cumulative = zeros(1, num_sample);                                     
+    
+    % shuffle
+    re_idx = randperm(num_sample);
+    X_train = train_X(re_idx,:);
+    Y_train = train_Y(re_idx,:);
+    
+    for t=1:epoch
+        tic;
+        for j=1:num_sample
+            if mod(j,1e3)==0
+                toc;
+                fprintf('%d iter(%d epoch)---processing %dth sample\n', i, t, j);
+                tic;
+            end
+            
+            X = X_train(j,:);
+            y = Y_train(j,:);
+            
+            y_predict = W*X' + b;
+            
+            % hinge loss
+            err = 1 - y * y_predict;
+            
+            % cumulative training hinge loss
+            idx = (t-1)*num_sample + j;
+            if idx == 1
+                loss_cumulative(idx) = max(0,err);
+            else
+                loss_cumulative(idx) = (loss_cumulative(idx-1) * (idx-1) + max(0,err))/idx;
+            end
+            
+            % record loss epoch-wise
+            loss_svm_train(i, t) = loss_cumulative(idx);
+            
+            % sgd update
+            if err > 0
+                W = W + y * X;
+                b = b + y;
+            end
+        end
+        
+        % validate epoch-wise
+        loss = 0.0;
+        correct_num = 0;
+        fprintf('validating\n');
+        tic;
+        [num_sample_test, ~] = size(test_X);
+        
+        for k=1:num_sample_test
+            
+            if mod(k,1e5)==0
+                fprintf('%d epoch(validation)---processing %dth sample\n',i, k);
+            end
+            
+            X = test_X(k,:);
+            y = test_Y(k,:);
+            
+            y_predict = W*X' + b;
+            err = 1 - y * y_predict;
+            loss = loss + max(0, err);
+            
+            % accuracy
+            if (y_predict>=0 && y==1) || (y_predict<0&&y==-1)
+                correct_num = correct_num + 1;
+            end
+        end
+        
+        % record test hinge loss epoch-wise
+        loss_svm_test(i, t) = loss / num_sample_test;
+        
+        % record test accuracy epoch-wise
+        accuracy_svm(i,t) = correct_num / num_sample_test;
+        
+        toc;
+        fprintf('validation done\n');
+       
+    end
+end
+
+
+%% plot cumulative learning curve
+plot(loss_cumulative, 'DisplayName', 'Linear SVM');
+legend('-DynamicLegend');
+xlabel('Number of samples seen');
+ylabel('Hinge loss');
+grid on;
+
+
---
+++ load_data.m
@@ -0,0 +1,9 @@
+% IJCNN data
+ijcnn_train = 'data/ijcnn/train_data';
+ijcnn_test = 'data/ijcnn/test_data';
+
+training_data = ijcnn_train;
+test_data = ijcnn_test;
+
+load(training_data);
+load(test_data);
